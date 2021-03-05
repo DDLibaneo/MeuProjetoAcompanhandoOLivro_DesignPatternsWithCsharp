f3332d5ce7965bcabc4d388217aa4a217f8d0874
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2021-03-04 22:17:47 -0300

    add esqueleto do teste AddInventoryCommand_Successful

--- FlixOne.InventoryManagementTests/UnitTest1.cs
+++ FlixOne.InventoryManagementTests/UnitTest1.cs
@@ -6,8 +6,16 @@ namespace FlixOne.InventoryManagementTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddInventoryCommand_Successful()
         {
+            // create an instance of the command
+            // add a new book with parameter "name"
+            // verify the book was added with the given name 
+            // with 0 quantity
+
+            Assert.Inconclusive("AddInventoryCommand_Successful has not been implemented.");
+
+
         }
     }
 }
