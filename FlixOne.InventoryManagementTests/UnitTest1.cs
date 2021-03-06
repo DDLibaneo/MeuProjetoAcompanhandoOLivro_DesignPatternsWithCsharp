bf1dee09c92d5832ae0a924aac1d873150a39f84
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2021-03-06 20:49:32 -0300

    add test placeholders for functionality

--- FlixOne.InventoryManagementTests/UnitTest1.cs
+++ FlixOne.InventoryManagementTests/UnitTest1.cs
@@ -14,8 +14,30 @@ namespace FlixOne.InventoryManagementTests
             // with 0 quantity
 
             Assert.Inconclusive("AddInventoryCommand_Successful has not been implemented.");
+        }
 
+        [TestMethod]
+        public void GetInventoryCommand_Successful()
+        {
+            Assert.Inconclusive("GetInventoryCommand_Successful has not been implemented.");
+        }
 
+        [TestMethod]
+        public void HelpCommand_Successful()
+        {
+            Assert.Inconclusive("HelpCommand_Successful has not been implemented.");
+        }
+
+        [TestMethod]
+        public void QuitCommand_Successful()
+        {
+            Assert.Inconclusive("QuitCommand_Successful has not been implemented.");
+        }
+
+        [TestMethod]
+        public void UpdateQuantity_Successful()
+        {
+            Assert.Inconclusive("UpdateQuantity_Successful has not been implemented.");
         }
     }
 }
