a92a173a5675b7a9eddc66751cd9c183080a7efc
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2021-03-09 22:16:35 -0300

    add HelpCommand and QuitCommand

---
+++ FlixOne.InventoryManagement/HelpCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FlixOne.InventoryManagement
+{
+    public class HelpCommand
+    {
+        public bool RunCommand(out bool shouldQuit)
+        {
+            Console.WriteLine("USAGE:");
+
+            // ...
+
+            Console.WriteLine("\taddinventory(a)");
+
+            // ...
+
+            Console.WriteLine("Examples:");
+
+            shouldQuit = false;
+
+            return true;
+        }
+    }    
+}
---
+++ FlixOne.InventoryManagement/QuitCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FlixOne.InventoryManagement
+{
+    public class QuitCommand
+    {
+        public bool RunCommand(out bool shouldQuit)
+        {
+            Console.WriteLine("Thank you for using FlixOne Inventory Management System");
+
+            shouldQuit = false;
+
+            return true;
+        }
+    }
+
+
+}
