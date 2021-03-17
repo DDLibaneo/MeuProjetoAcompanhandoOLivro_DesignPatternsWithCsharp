19e7d1a5a914b053d4aad877564761f0303f4be9
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2021-03-16 22:02:45 -0300

    add InventoryCommand

---
+++ FlixOne.InventoryManagement/InventoryCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlixOne.InventoryManagement
+{
+    public abstract class InventoryCommand
+    {
+        private readonly bool _isTerminatingCommand;
+
+        // O construtor é para as classes que vão herdar desta classe
+        // abstrata.
+        internal InventoryCommand(bool commandIsTerminating)
+        {
+            _isTerminatingCommand = commandIsTerminating;
+        }
+
+        public bool RunCommmand(out bool shouldQuit)
+        {
+            shouldQuit = _isTerminatingCommand;
+
+            return InternalCommand();
+        }
+
+        internal abstract bool InternalCommand();
+    }
+}
